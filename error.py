27337c335b0bd298504d77e316871dc10b46581e
Lonartie <gierschner.lg@gmail.com>
2023-05-06 16:19:02 +0200

    feat: add error msgs for parser

--- error.py
+++ error.py
@@ -6,3 +6,20 @@ class LexerError(Exception):
 
     def __str__(self):
         return f"LexerError: Expected {self.expected}, found {self.found} at position {self.position}"
+
+    def __repr__(self):
+        return str(self)
+
+
+class ParserError(Exception):
+    def __init__(self, found, expected, position, rule):
+        self.found = found
+        self.expected = expected
+        self.position = position
+        self.rule = rule
+
+    def __str__(self):
+        return f"ParserError: Expected {self.expected}, found {self.found} at position {self.position} in rule {self.rule}"
+
+    def __repr__(self):
+        return str(self)
--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
+from error import ParserError
 from tokens import *
-from ast import *
+from asts import *
 from lexer import *
 from typing import Optional
 
@@ -8,6 +9,10 @@ class Parser:
     __input__: list[Token]
     __current_pos__: int
     __current_token__: Token
+    __furthest_pos__: int
+    __furthest_token__: Token
+    __furthest_rule__: str
+    __furthest_expected__: str
 
     # Hilfsfunktionen
 
@@ -26,16 +31,31 @@ class Parser:
         else:
             return False
 
+    def __failed__(self, expected: str, rule: str) -> None:
+        if self.__current_pos__ > self.__furthest_pos__:
+            self.__furthest_pos__ = self.__current_pos__
+            self.__furthest_token__ = self.__current_token__
+            self.__furthest_rule__ = rule
+            self.__furthest_expected__ = expected
+
     def parse(self, tokens: list[Token]) -> Optional[Ast]:
         self.__input__ = tokens
         self.__current_pos__ = 0
         self.__current_token__ = self.__input__[self.__current_pos__]
+        self.__furthest_pos__ = 0
+        self.__furthest_token__ = self.__input__[self.__furthest_pos__]
+        self.__furthest_rule__ = ""
+        self.__furthest_expected__ = ""
+
         if (exp := self.expr()) and self.__input__[self.__current_pos__].type == EOF:
             return Ast(exp)
+        elif exp and self.__input__[self.__current_pos__].type != EOF:
+            raise ParserError(self.__current_token__, "EOF", self.__current_pos__, "parse")
         else:
-            return None
+            raise ParserError(self.__furthest_token__, self.__furthest_rule__, self.__furthest_pos__, self.__furthest_rule__)
 
-    # Grammatikregeln
+
+# Grammatikregeln
 
     def expr(self) -> Optional[Node]:
         return self.pow()
@@ -43,12 +63,16 @@ class Parser:
     def pow(self) -> Optional[Node]:
         pos = self.__current_pos__
 
-        if (lhs := self.mul()) and self.__expect__(MUL) and self.__expect__(MUL) and (rhs := self.expr()):
+        if (lhs := self.mul()) and (b1 := self.__expect__(MUL)) and (b2 := self.__expect__(MUL)) and (rhs := self.expr()):
             return Node(AST_POW, children=[lhs, rhs])
+        else:
+            self.__failed__("expr", "pow")
         self.__reset_pos__(pos)
 
         if mul := self.mul():
             return mul
+        else:
+            self.__failed__("mul", "pow")
         self.__reset_pos__(pos)
 
         return None
@@ -58,10 +82,14 @@ class Parser:
 
         if (lhs := self.add()) and self.__expect__(MUL) and (rhs := self.expr()):
             return Node(AST_MUL, children=[lhs, rhs])
+        else:
+            self.__failed__("expr", "mul")
         self.__reset_pos__(pos)
 
         if add := self.add():
             return add
+        else:
+            self.__failed__("add", "mul")
         self.__reset_pos__(pos)
 
         return None
@@ -70,10 +98,14 @@ class Parser:
         pos = self.__current_pos__
         if (lhs := self.term()) and self.__expect__(ADD) and (rhs := self.expr()):
             return Node(AST_ADD, children=[lhs, rhs])
+        else:
+            self.__failed__("expr", "add")
         self.__reset_pos__(pos)
 
         if term := self.term():
             return term
+        else:
+            self.__failed__("term", "add")
         self.__reset_pos__(pos)
 
         return None
@@ -82,10 +114,14 @@ class Parser:
         pos = self.__current_pos__
         if self.__expect__(USUB) and (rhs := self.factor()):
             return Node(AST_USUB, children=[rhs])
+        else:
+            self.__failed__("factor", "term")
         self.__reset_pos__(pos)
 
         if factor := self.factor():
             return factor
+        else:
+            self.__failed__("factor", "term")
         self.__reset_pos__(pos)
 
         return None
@@ -94,14 +130,20 @@ class Parser:
         pos = self.__current_pos__
         if self.__expect__(NUM):
             return Node(AST_NUM, value=self.__input__[self.__current_pos__ - 1].value)
+        else:
+            self.__failed__("number", "factor")
         self.__reset_pos__(pos)
 
         if self.__expect__(FLOAT):
             return Node(AST_FLOAT, value=self.__input__[self.__current_pos__ - 1].value)
+        else:
+            self.__failed__("float", "factor")
         self.__reset_pos__(pos)
 
         if self.__expect__(LPAREN) and (exp := self.expr()) and self.__expect__(RPAREN):
             return exp
+        else:
+            self.__failed__("expr", "factor")
         self.__reset_pos__(pos)
 
         return None
@@ -110,7 +152,7 @@ class Parser:
 # Parser starten
 
 if __name__ == '__main__':
-    text = "(1 + 2) * 3 ** 4"
+    text = "(1 ++ 2 )"
     parser = Parser()
     lexer = Lexer()
     token_list = lexer.lex(text)
@@ -119,4 +161,3 @@ if __name__ == '__main__':
     print(text)
     print(ast)
     print("evaluates to: ", ast.eval())
-
