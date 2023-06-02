c9880bff6cb9aed824b5465025199c8ac943a607
basharkhoulani <uk083781@uni-kassel.de>
2023-06-02 12:33:33 +0200

    feat: tryout anpassen

--- HA04/01/try_out.py
+++ HA04/01/try_out.py
@@ -24,3 +24,14 @@ print("===============================")
 
 ast_after_select_instrs = compiler.select_instructions(ast_after_rco)
 print(ast_after_select_instrs)
+
+print("===============================")
+
+ast_after_assign_registers = compiler.assign_homes(ast_after_select_instrs)
+print(ast_after_assign_registers)
+
+print("===============================")
+
+ast_after_patch_instructions = compiler.patch_instructions(ast_after_assign_registers)
+print(ast_after_patch_instructions)
+
